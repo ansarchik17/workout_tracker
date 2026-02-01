55bb9bd00c8ba1005cfa8456af89025169576577
Ansar <kelesansar62@gmail.com>
2026-02-02 00:30:33 +0500

    registration user

--- models/registerUserDTO.go
+++ models/registerUserDTO.go
@@ -1,7 +1,7 @@
 package models
 
 type RegisterUserRequest struct {
-	Name     string `json:"name"`
+	Name     string `json:"name" validate:"required,min=3"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
